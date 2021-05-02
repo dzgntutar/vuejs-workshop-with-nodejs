445e6303f48f990e66c18efef1cabac82feccaca
dutarrr <duzguntutar@gmail.com>
2021-05-02 13:01:13 +0300

    component design

--- ui/src/components/user/user.vue
+++ ui/src/components/user/user.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="container">
+  <div id="content" class="container border border-primary">
     <div class="row">
-      <div><user-list></user-list></div>
-      <div><user-detail></user-detail></div>
+      <p>
+        Parent Component Count : {{ count }}Parent Component Count : {{ count }}
+      </p>
+    </div>
+    <div class="row">
+      <div class="col-md-6 border border-secondary">
+        <user-list></user-list>
+      </div>
+      <div class="col-md-6 border border-danger">
+        <user-detail></user-detail>
+      </div>
     </div>
   </div>
 </template>
@@ -10,11 +19,23 @@
 <script>
 import UserList from "./userList";
 import UserDetail from "./userDetail";
+03;
 
 export default {
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
   components: {
     userList: UserList,
     userDetail: UserDetail,
   },
 };
 </script>
+
+<style scoped>
+#content {
+  padding: 30px;
+}
+</style>
