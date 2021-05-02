b0635ec55da820c30fe8586da2d799ccc94e0613
dutarrr <duzguntutar@gmail.com>
2021-05-02 13:10:14 +0300

    button click

--- ui/src/components/user/user.vue
+++ ui/src/components/user/user.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="content" class="container border border-primary">
     <div class="row">
-      <p>
-        Parent Component Count : {{ count }}Parent Component Count : {{ count }}
-      </p>
+      <p>Parent Component Count : {{ count }}</p>
+      <div>
+        <button class="btn btn-primary" @click="arttir">Arttır</button>
+        {{ "|" }}
+        <button class="btn btn-primary" @click="azalt">Azalt</button>
+      </div>
     </div>
+    <br />
     <div class="row">
       <div class="col-md-6 border border-secondary">
         <user-list></user-list>
@@ -19,13 +23,20 @@
 <script>
 import UserList from "./userList";
 import UserDetail from "./userDetail";
-03;
 
 export default {
   data: function () {
     return {
       count: 0,
     };
+  },
+  methods: {
+    arttir: function () {
+      this.count++;
+    },
+    azalt: function () {
+      this.count--;
+    },
   },
   components: {
     userList: UserList,
