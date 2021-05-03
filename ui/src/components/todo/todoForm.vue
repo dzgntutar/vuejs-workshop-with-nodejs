0f0c767a2bd19289dc4672214179239473cd7e1e
dutarrr <duzguntutar@gmail.com>
2021-05-03 23:09:02 +0300

    button submit

--- ui/src/components/todo/todoForm.vue
+++ ui/src/components/todo/todoForm.vue
@@ -1,35 +1,38 @@
 <template>
-  <div class="row">
-    <form>
-      <div class="form-group row">
-        <label for="title" class="col-sm-2 col-form-label">Title</label>
-        <div class="col-sm-10">
-          <input
-            type="text"
-            class="form-control"
-            id="title"
-            placeholder="Title"
-          />
-        </div>
+  <form @submit.prevent="SaveTodo()">
+    <div class="form-group">
+      <label for="title" class="col-sm-2 col-form-label">Title</label>
+      <div class="col-sm-10">
+        <input
+          type="text"
+          class="form-control"
+          id="title"
+          placeholder="Title"
+        />
       </div>
-      <div class="form-group row">
-        <label for="description" class="col-sm-2 col-form-label"
-          >Description</label
-        >
-        <div class="col-sm-10">
-          <textarea
-            id="description"
-            cols="30"
-            rows="10"
-            class="form-control"
-            placeholder="Description"
-          ></textarea>
-        </div>
+    </div>
+    <div class="form-group">
+      <label for="description" class="col-sm-2 col-form-label"
+        >Description</label
+      >
+      <div class="col-sm-10">
+        <textarea
+          id="description"
+          cols="30"
+          rows="10"
+          class="form-control"
+          placeholder="Description"
+        ></textarea>
       </div>
-    </form>
-  </div>
+    </div>
+    <button type="submit" class="btn btn-primary btn-sm">Sign in</button>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    SaveTodo() {},
+  },
+};
 </script>
