251f73b9468ba58aeffb974667205dc07145cd4e
dutarrr <duzguntutar@gmail.com>
2021-05-15 22:44:33 +0300

    template for app

--- 5-all-future/src/App.vue
+++ 5-all-future/src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>selam</div>
+  <div class="">
+    <app-header></app-header>
+    <app-product-purchase></app-product-purchase>
+    <app-footer></app-footer>
+  </div>
 </template>
 
 <script>
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
+import ProductPurchase from "./components/products/ProductPurchase";
 export default {
-  components: {},
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+    appProductPurchase: ProductPurchase,
+  },
 };
 </script>
 
