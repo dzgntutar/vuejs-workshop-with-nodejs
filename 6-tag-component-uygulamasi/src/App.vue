9c7a710e8c48a6fcd172a4b356a950c2d439dd7f
dutarrr <duzguntutar@gmail.com>
2021-05-22 23:13:53 +0300

    ..

--- 6-tag-component-uygulamasi/src/App.vue
+++ 6-tag-component-uygulamasi/src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="container">
-    <app-tags v-model="tags" color="success"/>
-    <app-tags v-model="tags2" color="danger"/>
-    <app-tags v-model="tags2" color="info"/>
-    <app-tags v-model="tags2" color="primary"/>
-    <app-tags v-model="tags2" color="warning"/>
-    <app-tags v-model="tags2" color="secondary"/>
+    <app-tags v-model="tags" color="success" />
+    <app-tags v-model="tags2" color="danger" />
+    <app-tags v-model="tags2" color="info" />
+    <app-tags v-model="tags2" color="primary" />
+    <app-tags v-model="tags2" color="warning" />
+    <app-tags v-model="tags2" color="secondary" />
   </div>
 </template>
 <script>
-  import Tags from "./components/Tags"
-  export default {
-    components : {
-      appTags : Tags
-    },
-    data(){
-      return {
-        tags : "deneme,test,gkandemir",
-        tags2 : "deneme,test,gkandemir,klavye,telsiz",
-      }
-    }
-  }
+import Tags from "./components/Tags";
+export default {
+  components: {
+    appTags: Tags,
+  },
+  data() {
+    return {
+      tags: "aaa,bbb,ccc,dddd",
+      tags2: "Boş",
+    };
+  },
+};
 </script>
 <style>
-  body {
-    font-family: sans-serif;
-  }
+body {
+  font-family: sans-serif;
+}
 </style>
